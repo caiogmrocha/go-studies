883db6f820a2533247373a5d67ec8eb105b266cc
caiogmrocha <caioguilherme739@gmail.com>
2024-10-14 22:44:11 -0300

    chore: add github.com/joho/godotenv

--- 4-cli-database-crud/main.go
+++ 4-cli-database-crud/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -15,7 +18,16 @@ type Product struct {
 }
 
 func main() {
-	dsn := "root:password@tcp(0.0.0.0:3306)/public?charset=utf8mb4&parseTime=True&loc=Local"
+	godotenv.Load()
+
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_DEFAULT"),
+	)
   db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
